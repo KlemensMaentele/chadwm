19491aff96f0f6faffd13473ea1b0885cd6617d1
KlemensMaentele <klemens.maentele@gmail.com>
2023-07-15 17:50:17 +0200

    Removes setting keyboard options Was initialy implemented like this because I used a local config file to set some keyboard setting which I now do in a global config file as described in: https://github.com/KlemensMaentele/dotfiles#keyboard

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -150,7 +150,7 @@ static Key keys[] = {
     { MODKEY,                           XK_Return,  spawn,          {.v = term }},
     // { MODKEY,                       XK_Return, spawn,          SHCMD("st_pad && st")},
     { MODKEY,                           XK_w,       spawn,          {.v = browser }},
-    { MODKEY|ControlMask,               XK_space,   spawn,          SHCMD("/usr/bin/setxkbmap -query | grep 'layout:[[:blank:]]*us' && setxkbmap de || setxkbmap us && xmodmap ~/.config/.Xmodmap")},
+    { MODKEY|ControlMask,               XK_space,   spawn,          SHCMD("/usr/bin/setxkbmap -query | grep 'layout:[[:blank:]]*us' && setxkbmap de || setxkbmap us")},
     { MODKEY,                           XK_r,       spawn,          SHCMD("st -e zsh -c /home/klemens/devel/scripts/lfub")},
     { MODKEY,                           XK_m,       spawn,          SHCMD("audacious")},
     { MODKEY,                           XK_p,       spawn,          SHCMD("st -e pulsemixer")},
